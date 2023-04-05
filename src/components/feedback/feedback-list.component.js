1948998ea439192fbe0bdea7a5fa0a25bd29f1fb
kalanarathnayake <kalanagayanga8@gmail.com>
2023-04-05 22:53:40 +0530

    Feat: Feedback add UI updated

--- src/components/feedback/feedback-list.component.js
+++ src/components/feedback/feedback-list.component.js
@@ -1,205 +1,217 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import jsPDF from 'jspdf'; 
+import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-import {Button, Form, Table, ButtonGroup, Modal, Row, Col, InputGroup} from "react-bootstrap";
-
 
 const Feedback = props => (
-    <tr>
-       
-        <td>{props.feedback.feedback}</td>
-        <td>{props.feedback.instructor}</td>
-     
-       
-        {/* <td>
-            
-            <button  onClick ={() => {props.gotoUpdateInstructor(props.instructor._id)}  }>
-               
-                    Update
-                   
+    <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+        <td className='px-6 py-4 text-base font-medium text-blue-950'>{props.feedback.feedback}</td>
+        <td className='px-6 py-4'>{props.feedback.instructor}</td>
+        <td className='px-6 py-4'>
+            <div class="flex justify-center">
+                <div class="">
+                    <button className='inline-flex items-center px-4 py-2 ml-1 text-sm font-medium text-white duration-100 bg-indigo-500 rounded-md hover:bg-blue-200' onClick={() => {  }}>
+                        <div class="">
+                            <svg class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round " stroke-width="2" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"></path>
+                            </svg>
+                        </div>
+                        <div class="">
+                            Update
+                        </div>
                     </button>
-            <button  onClick ={() => {props.deleteInstructor(props.instructor._id)}}>Delete</button>
-            
-                                                        
-                                                  
-        </td> */}
+                </div>
+                <div class="">
+                    <button className='inline-flex items-center px-4 py-2 ml-1 text-sm font-medium text-white duration-100 bg-red-500 rounded-md hover:bg-red-200' onClick={() => {}}>
+                        <div class="">
+                            <svg class="h-5 w-5 mr-2 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </div>
+                        <div class="">
+                            Delete
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </td>
     </tr>
 )
 
 export class FeedbackList extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-
         // this.deleteInstructor = this.deleteInstructor.bind(this);
         // this.gotoUpdateInstructor = this.gotoUpdateInstructor.bind(this);
-
-        this.state = {feedback : [], instructor : [],
-            searchFeedback : ""};
+        this.state = {
+            feedback: [], instructor: [],
+            searchFeedback: ""
+        };
     }
-
 
     componentDidMount() {
         this.getInstructors();
         this.refreshTable();
-        }
+    }
 
-        refreshTable() {
-            axios.get('http://localhost:5000/feedback/')
+    refreshTable() {
+        axios.get('http://localhost:5000/feedback/')
             .then(response => {
-                this.setState({ feedback : response.data })
+                this.setState({ feedback: response.data })
             })
             .catch((error) => {
                 console.log(error);
             })
-            }
-    
-            // gotoUpdateInstructor = (id) => {
-            //     this.setState({
-            //         id: id,
-            //         show: true
-                    
-            //     })
-            //     console.log("LIst id is :" +id);
-            // }
-
-             //Modal box
-    // 
-
-    getInstructors(){
-        axios.get('http://localhost:5000/instructor/')
-        .then(response => {
-            this.setState({ instructor : response.data })
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-        
-
     }
 
-        feedbackList(){
-            return this.state.feedback.map(currentfeedback => {
-                return <Feedback feedback = {currentfeedback} />;
+    // gotoUpdateInstructor = (id) => {
+    //     this.setState({
+    //         id: id,
+    //         show: true
+
+    //     })
+    //     console.log("LIst id is :" +id);
+    // }
+
+    //Modal box
+    // 
+
+    getInstructors() {
+        axios.get('http://localhost:5000/instructor/')
+            .then(response => {
+                this.setState({ instructor: response.data })
             })
-        }
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
-    
-        searchFeedbackList(){
+    feedbackList() {
+        return this.state.feedback.map(currentfeedback => {
+            return <Feedback feedback={currentfeedback} />;
+        })
+    }
 
-            return this.state.feedback.map((currentfeedback) => {
-                if (
-                    this.state.searchFeedback ==
-                    currentfeedback.instructor
-                ){
-                    return (
-                        <tr>
-                       
+    searchFeedbackList() {
+        return this.state.feedback.map((currentfeedback) => {
+            if (
+                this.state.searchFeedback ==
+                currentfeedback.instructor
+            ) {
+                return (
+                    <tr>
                         <td style={{ width: "10%" }}>{currentfeedback.feedback}</td>
                         <td style={{ width: "10%" }}>{currentfeedback.instructor}</td>
-                       
-                        </tr>
-                    );
-                }
-            });
-        }
+                    </tr>
+                );
+            }
+        });
+    }
 
+    exportFeedback = () => {
+        console.log("Exporting PDF")
+        const unit = "pt";
+        const size = "A3";
+        const orientation = "landscape";
+        const marginLeft = 40;
+        const doc = new jsPDF(orientation, unit, size);
+        const title = "Feedback List Report ";
+        const headers = [["Feedback", "Instructor"]];
+        const fed = this.state.feedback.map(
+            Feedback => [
+                Feedback.feedback,
+                Feedback.instructor,
+            ]
+        );
 
-        exportFeedback = () => {
-            console.log( "Exporting PDF" )
-    
-    
-            const unit = "pt";
-            const size = "A3"; 
-            const orientation = "landscape"; 
-            const marginLeft = 40;
-            const doc = new jsPDF( orientation, unit, size );
-    
-            const title = "Feedback List Report ";
-            const headers = [["Feedback","Instructor"]];
-    
-            const fed = this.state.feedback.map(
-                Feedback=>[
-                    Feedback.feedback,
-                    Feedback.instructor,
-                   
-                ]
-            );
-    
-            let content = {
-                startY: 50,
-                head: headers,
-                body:fed
-            };
-            doc.setFontSize( 20 );
-            doc.text( title, marginLeft, 40 );
-            require('jspdf-autotable');
-            doc.autoTable( content );
-            doc.save( "Feedback-list.pdf" )
-        }
-
+        let content = {
+            startY: 50,
+            head: headers,
+            body: fed
+        };
+        doc.setFontSize(20);
+        doc.text(title, marginLeft, 40);
+        require('jspdf-autotable');
+        doc.autoTable(content);
+        doc.save("Feedback-list.pdf")
+    }
 
     render() {
         return (
-            <div >
-            <div>
-            
-            
-                <table >
-                    <tr>
-                        <th><h3>Feedback Details</h3></th>
-                        <td><button ><Link to = {"/creatFeedback" }>Add Feedback</Link></button>
-                        <button onClick={() => this.exportFeedback()}>
-                       
-                        Download Report Here</button></td>
-                    </tr>
+            <div className="flex flex-col px-5 pt-2">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                        <div className='items-center overflow-hidden'>
+                            <div class="grid grid-cols-1 gap-4 content-start">
+                                <table className='mt-2 mb-4'>
+                                    <tr>
+                                        <th className='drop-shadow-md'>
+                                            <p className='text-3xl uppercase'>Feedback Details</p>
+                                        </th>
+                                        <td className='flex justify-end gap-2'>
+                                            <div class="flex justify-end sm:flex-row sm:text-left sm:justify-end gap-2">
+                                                <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                                    <Link className='font-semibold text-white no-underline' to={"/creatFeedback"}>
+                                                        <div class="flex">
+                                                            <div class="">
+                                                                <svg class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                                                                </svg>
+                                                            </div>
+                                                            <div class="">
+                                                                Add Feedback
+                                                            </div>
+                                                        </div>
+                                                    </Link>
+                                                </button>
+                                                <button class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => this.exportFeedback()}>
+                                                    <div class="">
+                                                        <svg class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                                        </svg>
+                                                    </div>
+                                                    <div class="">
+                                                        Download Report Here
+                                                    </div>
+                                                </button>
+                                            </div>
+                                            <div class="flex justify-end sm:flex-row sm:text-left sm:justify-end">
+                                                <input
+                                                    className="border-2 border-blue-700 font-semibold form-control rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                                                    type="text"
+                                                    placeholder="Search by Instructor Name"
+                                                    aria-label="Search"
+                                                    onChange={(e) => {
+                                                        this.setState({
+                                                            searchFeedback: e.target.value
+                                                        });
+                                                    }}
+                                                />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
 
-                    <div >
-                    <input style={{ width: "250px", marginTop:"10px"}}
-                    class="form-control"
-                    type="text"
-                    placeholder="Search by Instructor Name"
-                    aria-label="Search"
-                    onChange={(e) => {
-                        this.setState({
-                        searchFeedback: e.target.value
-                        });
-                    }}
-                    />
+                            <div className='relative grid content-start grid-cols-1 gap-4 overflow-x-auto shadow-md sm:rounded-lg'>
+                                <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400' >
+                                    <thead className='p-5 text-xs text-gray-700 uppercase border bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                                        <tr>
+                                            <th className="p-2 border-black tbhead">Feedback</th>
+                                            <th className="p-2 tbhead">Instructor Name</th>
+                                            <th className="p-2 text-center tbhead">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.searchFeedback == "" ? this.feedbackList() : this.searchFeedbackList()}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-                </table>
-            
-            
-                
-                <table class="table table-bordered">
-                <thead >
-                    <tr>
-                        
-                        <th className = "tbhead">Feedback</th>
-                        <th className = "tbhead">Instructor Name</th>
-                       
-                        
-                        
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    { this.state.searchFeedback == "" ? this.feedbackList() : this.searchFeedbackList() }
-                </tbody>
-            </table>
-           
-           
-        </div>
-
-          {/*--------------------------Model Box to Edit Conference--------------------------*/}
-
-       
-
-                  {/*----------------------------------------------------*/}
-
-        </div>
         )
     }
 }
